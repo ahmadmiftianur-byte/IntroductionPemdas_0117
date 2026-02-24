b417f149eca6aa1cc7c86aab69778743c9443a8c
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-02-24 19:03:51 +0700

    int main

--- file1.cpp
+++ file1.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std; 
+int main() {
+    cout << "kalkulator luas" << endl;
+    cout << "lebar=" << endl;
+    int lebar=0;
+    cin >> lebar;
+    cout << "panjang=" << endl;
+    int panjang=0;
+    cin >> panjang;
+    int luas = lebar * panjang;
+    cout << "luas=" << luas << endl;
+
+}
