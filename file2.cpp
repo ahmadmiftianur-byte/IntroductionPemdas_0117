e71678a014ff37441783377bf6b0eaf587fea809
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-02-24 19:39:53 +0700

    menambahkan CIN

---
+++ file2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int p, l, luas;//deklarasi 
+
+    //langkah 2
+    cout << "Masukkan panjang : ";
+    cin >> p;   
+
+    //langkah 3
+    cout << "Masukkan lebar : ";    
+    cin >> l;   
+
+    //langkah 4
+    luas = p * l;   
+
+    //langkah 5
+
+    cout << "Luas persegi panjang : " << luas;
