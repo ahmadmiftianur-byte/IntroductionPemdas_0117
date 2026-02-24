c57a5efffd64ca873b3db69413160b62bfa4b662
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-02-24 19:31:12 +0700

    menambahkan dan memperbaiki int

--- file1.cpp
+++ file1.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 using namespace std; 
 int main() {
-    cout << "kalkulator luas" << endl;
-    cout << "lebar=" << endl;
-    int lebar=0;
-    cin >> lebar;
-    cout << "panjang=" << endl;
-    int panjang=0;
-    cin >> panjang;
-    int luas = lebar * panjang;
-    cout << "luas=" << luas << endl;
+    
+    int umur = 20;
+    umur = 20;
 
-    cout << "selamat menunaikan ibadah puasa" << endl;
-    cout << "BY ahmy sang KAISAR" << endl;
-}
+    
